b62750f86f5d00b123ae90d2b180174d1d5b07b4
Gytis <33606253+gytuksas@users.noreply.github.com>
2024-03-22 20:23:12 +0200

    Basic implementation of product page & grids for displaying categories and products.

---
+++ BombasticElectronics/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace BombasticElectronics.Entities
+{
+    public class Category
+    {
+        public string name { get; set; }
+        public string image { get; set; }
+
+        public Category(string name, string image)
+        {
+            this.name = name;
+            this.image = image;
+        }
+    }
+}
---
+++ BombasticElectronics/wwwroot/js/sideScrollCategory.js
@@ -0,0 +1,13 @@
+﻿let scrollContainer = document.querySelector('.category-grid');
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("fwdBtn");
+
+nextBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = 'smooth';
+    scrollContainer.scrollLeft += 256;
+});
+
+backBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = 'smooth';
+    scrollContainer.scrollLeft -= 256;
+});
